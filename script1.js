b75c050a5dc07b172a81618eb6fe6f3aa5443562
girinathchickoo <girinathgiri24@gmail.com>
2020-09-26 13:48:53 +0530

    adding project

---
+++ script1.js
@@ -0,0 +1,19 @@
+var color1=document.querySelector(".color1");
+var color2=document.querySelector(".color2");
+var h3=document.querySelector("h3");
+var body=document.getElementById("gradient");
+
+
+
+
+function setcolor(){
+
+	body.style.background= "linear-gradient(to right,"+ color1.value +","+ color2.value +")";
+   console.log(body.style);
+    
+   h3.textContent= body.style.background;
+}
+
+color1.addEventListener("input",setcolor);
+color2.addEventListener("input",setcolor);
+
