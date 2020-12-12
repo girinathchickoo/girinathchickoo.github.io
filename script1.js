a09e493af3604ccb9f419e587506cfcafc3975e4
girinathchickoo <67161043+girinathchickoo@users.noreply.github.com>
2020-12-12 19:54:49 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,7 +1,7 @@
 var color1=document.querySelector(".color1");
 var color2=document.querySelector(".color2");
 var h3=document.querySelector("h3");
-var body=document.getElementById("gradient");
+var body=document.getElementById("bg");
 
 
 
